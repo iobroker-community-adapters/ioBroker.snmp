4bfbbb961d497ef8c61e3926dace36a21d563978
CTJaeger <info@ct-j.de>
2017-10-21 07:02:41 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -12,17 +12,14 @@
 // you have to require the utils module and call adapter function
 var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 var snmp = require('net-snmp');
+var adapter = utils.adapter('sma-em');
 
 adapter.on('ready', function () {
     main();
 });
 
 adapter.on('unload', function () {
-    if (timer) {
-        clearInterval(timer);
-        timer = 0;
-    }
-    isStopping = true;
+  
 });
 
 
